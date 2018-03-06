91a58c9d20e304f67b6e613e7f1659d9470c4742
Christian Andrei <christianandreizpapa@gmail.com>
2018-03-06 15:47:09 +0800

    changes on package.json

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require("gulp");
+
+gulp.task("default" , function(){
+    console.log("Gulp Success!");
+});
+
+gulp.task("task" , function(){
+    console.log("I love programming");
+});
